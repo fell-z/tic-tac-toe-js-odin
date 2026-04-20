c66163b20812a0b68475757f6010e11620a9a667
fell-z <matheusfelipeagapito@hotmail.com>
2026-04-20 08:51:06 -0300

    Add a controller to play and display the game on the DOM
    Wrapped the entire code in an IIFE to expose no code to the global scope.

--- script.js
+++ script.js
@@ -1,168 +1,234 @@
-function GameBoard() {
-  const board = [];
+(function () {
+  function GameBoard() {
+    const board = [];
 
-  for (let i = 0; i < 3; ++i) {
-    board[i] = [];
+    const populate = () => {
+      for (let i = 0; i < 3; ++i) {
+        board[i] = [];
 
-    for (let j = 0; j < 3; ++j) board[i].push(null);
-  }
+        for (let j = 0; j < 3; ++j) board[i].push(null);
+      }
+    };
+    populate();
 
-  const getBoard = () => board;
+    const getBoard = () => board;
 
-  let remainingMarkers = 9;
+    let remainingMarkers = 9;
 
-  const getRemainingMarkers = () => remainingMarkers;
+    const getRemainingMarkers = () => remainingMarkers;
 
-  const isEmptyAt = (row, column) => {
-    return board[row][column] === null;
-  };
-
-  const getMarker = (row, column) => {
-    return board[row][column] ?? " ";
-  };
-
-  const putMarker = (row, column, marker) => {
-    board[row][column] = marker;
-    remainingMarkers--;
-  };
-
-  const render = () => {
-    const border = "+---+---+---+";
-
-    console.log(border);
-    for (let i = 0; i < 3; ++i) {
-      if (i === 1) console.log(border);
-
-      console.log(`| ${getMarker(i, 0)} | ${getMarker(i, 1)} | ${getMarker(i, 2)} |`);
-
-      if (i === 1) console.log(border);
-    }
-    console.log(border);
-  };
-
-  return {
-    getBoard,
-    getRemainingMarkers,
-    isEmptyAt,
-    getMarker,
-    putMarker,
-    render,
-  };
-}
-
-function GameController(playerOneName = "Player One", playerTwoName = "Player Two") {
-  const board = GameBoard();
-
-  const players = [
-    {
-      name: playerOneName,
-      marker: "X",
-    },
-    {
-      name: playerTwoName,
-      marker: "O",
-    },
-  ];
-  let activePlayer = players[0];
-
-  const getActivePlayer = () => activePlayer;
-  const switchActivePlayer = () => {
-    activePlayer = activePlayer === players[0] ? players[1] : players[0];
-  };
-
-  const isOutOfBounds = (row, column) => row < 0 || row > 2 || column < 0 || column > 2;
-
-  let winningLine = null;
-  const getWinningLine = () => winningLine;
-
-  const findWinningLine = (row, column) => {
-    const directions = {
-      horizontal: [
-        [0, -1],
-        [0, 1],
-      ],
-      vertical: [
-        [-1, 0],
-        [1, 0],
-      ],
-      // A => has a marker at left upper corner
-      // B => has a marker at right upper corner
-      diagonalA: [
-        [-1, -1],
-        [1, 1],
-      ],
-      diagonalB: [
-        [-1, 1],
-        [1, -1],
-      ],
+    const isEmptyAt = (row, column) => {
+      return board[row][column] === null;
     };
 
-    for (const [dirName, dirVectors] of Object.entries(directions)) {
-      let count = 1;
+    const getMarker = (row, column) => {
+      return board[row][column] ?? " ";
+    };
 
-      for (const vector of dirVectors) {
-        let currRow = row + vector[0];
-        let currColumn = column + vector[1];
+    const putMarker = (row, column, marker) => {
+      board[row][column] = marker;
+      remainingMarkers--;
+    };
 
-        while (!isOutOfBounds(currRow, currColumn)) {
-          if (board.getMarker(currRow, currColumn) === getActivePlayer().marker) {
-            count++;
-            if (count === 3) {
-              return { direction: dirName, row, column };
+    const reset = () => {
+      populate();
+      remainingMarkers = 9;
+    };
+
+    return {
+      getBoard,
+      getRemainingMarkers,
+      isEmptyAt,
+      getMarker,
+      putMarker,
+      reset,
+    };
+  }
+
+  function GameController(playerOneName, playerTwoName) {
+    const board = GameBoard();
+
+    const players = [
+      {
+        name: playerOneName,
+        marker: "X",
+      },
+      {
+        name: playerTwoName,
+        marker: "O",
+      },
+    ];
+    let activePlayer = players[0];
+
+    const getActivePlayer = () => activePlayer;
+    const switchActivePlayer = () => {
+      activePlayer = activePlayer === players[0] ? players[1] : players[0];
+    };
+
+    const isOutOfBounds = (row, column) => row < 0 || row > 2 || column < 0 || column > 2;
+
+    const isAWinningLine = (row, column) => {
+      const directions = {
+        horizontal: [
+          [0, -1],
+          [0, 1],
+        ],
+        vertical: [
+          [-1, 0],
+          [1, 0],
+        ],
+        diagonalA: [
+          [-1, -1],
+          [1, 1],
+        ],
+        diagonalB: [
+          [-1, 1],
+          [1, -1],
+        ],
+      };
+
+      for (const directionVectors of Object.values(directions)) {
+        let count = 1;
+
+        for (const vector of directionVectors) {
+          let currRow = row + vector[0];
+          let currColumn = column + vector[1];
+
+          while (!isOutOfBounds(currRow, currColumn)) {
+            if (board.getMarker(currRow, currColumn) === getActivePlayer().marker) {
+              count++;
+              if (count === 3) {
+                return true;
+              }
+
+              currRow += vector[0];
+              currColumn += vector[1];
+            } else {
+              break;
             }
-
-            currRow += vector[0];
-            currColumn += vector[1];
-          } else {
-            break;
           }
         }
       }
-    }
 
-    return null;
-  };
+      return false;
+    };
 
-  let gameOver = false;
-  const isGameOver = () => gameOver;
+    let gameOver = false;
+    const isGameOver = () => gameOver;
 
-  const isATie = () => board.getRemainingMarkers() === 0 && winningLine === null;
+    let gameTied = false;
+    const isATie = () => gameTied;
 
-  const startPlayerTurn = (row, column) => {
-    if (isGameOver()) {
-      console.log("The game is already over.");
-      return;
-    }
+    const startPlayerTurn = (row, column) => {
+      if (!isOutOfBounds(row, column) && board.isEmptyAt(row, column)) {
+        board.putMarker(row, column, getActivePlayer().marker);
+      } else {
+        return;
+      }
 
-    if (!isOutOfBounds(row, column) && board.isEmptyAt(row, column)) {
-      board.putMarker(row, column, getActivePlayer().marker);
-    } else {
-      console.log("Invalid position to put marker!");
-      return;
-    }
+      const playerWon = isAWinningLine(row, column);
+      gameTied = board.getRemainingMarkers() === 0 && !playerWon;
 
-    winningLine = findWinningLine(row, column);
+      if (playerWon || isATie()) {
+        gameOver = true;
+      } else {
+        switchActivePlayer();
+      }
+    };
 
-    if (winningLine !== null) {
-      gameOver = true;
-      console.log(`Congratulations, ${getActivePlayer().name}! You won!`);
-    } else if (isATie()) {
-      gameOver = true;
-      console.log("Tie!");
-    } else {
-      switchActivePlayer();
-      console.log(`Your turn, ${getActivePlayer().name}.`);
-    }
+    const reset = (playerOneName, playerTwoName) => {
+      board.reset();
+      players[0].name = playerOneName;
+      players[1].name = playerTwoName;
+      activePlayer = players[0];
+      gameOver = false;
+    };
 
-    board.render();
-  };
+    return {
+      getBoard: board.getBoard,
+      getActivePlayer,
+      isGameOver,
+      isATie,
+      startPlayerTurn,
+      reset,
+    };
+  }
 
-  return {
-    getBoard: board.getBoard,
-    getActivePlayer,
-    getWinningLine,
-    isGameOver,
-    isATie,
-    startPlayerTurn,
-  };
-}
+  function DisplayController() {
+    const game = GameController();
+
+    const boardElement = document.querySelector(".board");
+    const outputElement = document.querySelector("output");
+
+    const render = () => {
+      boardElement.textContent = "";
+
+      game.getBoard().forEach((row, rowIndex) => {
+        row.forEach((column, columnIndex) => {
+          const squareButton = document.createElement("button");
+
+          const markerGraphic = document.createElement("img");
+          markerGraphic.width = 100;
+          
+          if (column === "X") {
+            markerGraphic.src += "assets/cross.svg";
+          } else if (column === "O") {
+            markerGraphic.src += "assets/circle.svg";
+          }
+
+          if (markerGraphic.src.length !== 0) {
+            squareButton.appendChild(markerGraphic);
+          }
+
+          squareButton.type = "button";
+          squareButton.dataset.row = rowIndex;
+          squareButton.dataset.column = columnIndex;
+
+          boardElement.appendChild(squareButton);
+        });
+      });
+    };
+
+    document.getElementById("new-game-btn").addEventListener("click", () => {
+      boardElement.classList.remove("disabled");
+      game.reset(
+        document.querySelector('input[name="playerOneName"]').value,
+        document.querySelector('input[name="playerTwoName"]').value,
+      );
+      render();
+      outputElement.textContent = "";
+    });
+
+    boardElement.addEventListener("click", (e) => {
+      if (game.isGameOver()) {
+        return;
+      }
+
+      const row = e.target.dataset.row;
+      const column = e.target.dataset.column;
+
+      if (!row || !column) return;
+
+      game.startPlayerTurn(parseInt(row), parseInt(column));
+      render();
+
+      if (game.isGameOver()) {
+        const resultElement = document.createElement("p");
+
+        if (game.isATie()) {
+          resultElement.textContent = "Tie!";
+        } else {
+          resultElement.textContent = `Congratulations, ${game.getActivePlayer().name}, you won!`;
+        }
+
+        outputElement.appendChild(resultElement);
+
+        boardElement.classList.add("disabled");
+      }
+    });
+
+    render();
+  }
+
+  DisplayController();
+})();
